54a0b31800a4682628d737617de5c7a48469a013
Adam Jahn <ajjahn@gmail.com>
2012-08-07 15:55:34 -0400

    fix up the mpd::service class

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -1,5 +1,9 @@
 class mpd::server::service {
  service { "mpd":
     ensure => running,
+    hasstatus => true,
+    hasrestart => true,
+    enable => true,
+    require => Class["mpd::server::config"]
   }
 }
